7bbaf4ed4f7d6eaa5830b9d2fdcac673313e924d
Simon <simon.meggle@elabit.de>
2019-05-23 15:33:06 +0200

    Error Handling: skip pod if usedBytes are missing

--- ephemeral-export.py
+++ ephemeral-export.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from prometheus_client import start_http_server, Metric, REGISTRY
 from kubernetes import client,config
 import json
@@ -20,29 +22,30 @@ class JsonCollector(object):
         response = r.json()
 
         for resp in response['pods']:
-            podname = resp['podRef']['name']
-            namespace = resp['podRef']['namespace']
-            labels = {
-                'namespace' : namespace,
-                'pod'       : podname,
-                'node'      : nodename
-            }
-            ephem = resp['ephemeral-storage']
-            prefix = 'pod_ephemeralstorage'
+            if 'usedBytes' in resp['ephemeral-storage'].keys():
+              podname = resp['podRef']['name']
+              namespace = resp['podRef']['namespace']
+              labels = {
+                  'namespace' : namespace,
+                  'pod'       : podname,
+                  'node'      : nodename
+              }
+              ephem = resp['ephemeral-storage']
+              prefix = 'pod_ephemeralstorage'
 
-            ephem_usedBytes =       resp['ephemeral-storage']['usedBytes']
-            ephem_availableBytes =  resp['ephemeral-storage']['availableBytes']
-            ephem_capacityBytes =   resp['ephemeral-storage']['capacityBytes']
-            ephem_inodesFree =      resp['ephemeral-storage']['inodesFree']
-            ephem_inodes =          resp['ephemeral-storage']['inodes']
-            ephem_inodesUsed =      resp['ephemeral-storage']['inodesUsed']
+              ephem_usedBytes =       resp['ephemeral-storage']['usedBytes']
+              ephem_availableBytes =  resp['ephemeral-storage']['availableBytes']
+              ephem_capacityBytes =   resp['ephemeral-storage']['capacityBytes']
+              ephem_inodesFree =      resp['ephemeral-storage']['inodesFree']
+              ephem_inodes =          resp['ephemeral-storage']['inodes']
+              ephem_inodesUsed =      resp['ephemeral-storage']['inodesUsed']
 
-            yield self.add_metric(prefix, 'usage_bytes', ephem_usedBytes, labels, 'UsedBytes represents the bytes used for a specific task on the filesystem. This may differ from the total bytes used on the filesystem and may not equal CapacityBytes - AvailableBytes.')
-            yield self.add_metric(prefix, 'avail_bytes', ephem_availableBytes, labels, 'AvailableBytes represents the storage space available (bytes) for the filesystem.')
-            yield self.add_metric(prefix, 'avail_bytes_total', ephem_capacityBytes, labels, 'CapacityBytes represents the total capacity (bytes) of the filesystems underlying storage.')
-            yield self.add_metric(prefix, 'usage_inodes', ephem_inodesUsed, labels, 'InodesUsed represents the inodes used by the filesystem. This may not equal Inodes - InodesFree because this filesystem may share inodes with other "filesystems".')
-            yield self.add_metric(prefix, 'avail_inodes', ephem_inodesFree, labels, 'InodesFree represents the free inodes in the filesystem.')
-            yield self.add_metric(prefix, 'avail_inodes_total', ephem_inodes, labels, 'Inodes represents the total inodes in the filesystem.')
+              yield self.add_metric(prefix, 'usage_bytes', ephem_usedBytes, labels, 'UsedBytes represents the bytes used for a specific task on the filesystem. This may differ from the total bytes used on the filesystem and may not equal CapacityBytes - AvailableBytes.')
+              yield self.add_metric(prefix, 'avail_bytes', ephem_availableBytes, labels, 'AvailableBytes represents the storage space available (bytes) for the filesystem.')
+              yield self.add_metric(prefix, 'avail_bytes_total', ephem_capacityBytes, labels, 'CapacityBytes represents the total capacity (bytes) of the filesystems underlying storage.')
+              yield self.add_metric(prefix, 'usage_inodes', ephem_inodesUsed, labels, 'InodesUsed represents the inodes used by the filesystem. This may not equal Inodes - InodesFree because this filesystem may share inodes with other "filesystems".')
+              yield self.add_metric(prefix, 'avail_inodes', ephem_inodesFree, labels, 'InodesFree represents the free inodes in the filesystem.')
+              yield self.add_metric(prefix, 'avail_inodes_total', ephem_inodes, labels, 'Inodes represents the total inodes in the filesystem.')
 
   def add_metric(self, prefix, name, value, labels, summary):
     metric = Metric(prefix + '_' + name, summary, 'summary')
